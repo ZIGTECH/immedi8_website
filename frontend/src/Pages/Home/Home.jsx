75e0b058a9d9b69532e883858919804353c5ebbe
maddsic <sainsidibeh@yahoo.com>
2022-08-12 00:43:33 +0000

    update

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { About } from "../../components/About/About";
 import Header from "../../components/Header/Header";
+import { Highlight } from "../../components/Highhight/Highlight";
 import { Services } from "../../components/Services/Services";
 
 const serviceStyle = {
 	background: "#222121",
+};
+const highlightStyle = {
+	background: "#eef4ed",
 };
 
 export const Home = () => {
 	return (
 		<div className="">
 			<Header />
+
+			<section className="bg-slate-200">
+				<Highlight />
+			</section>
 
 			<section className="container mx-auto mt-20">
 				<About />
