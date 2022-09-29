1e6f1b5f44dbf7f355ff8725c811ec239ff90f0a
David Ladipo <deedevs21@gmail.com>
2022-09-29 17:11:31 +0000

    db, build

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "13.41.219.189"),
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "immedi8_website_db"),
-      user: env("DATABASE_USERNAME", "admin"),
+      user: env("DATABASE_USERNAME", "dbuser"),
       password: env("DATABASE_PASSWORD", "password123"),
       ssl: env.bool("DATABASE_SSL", false),
     },
